b528dc1b63483591807eb3ce3dd0198d63d20406
HussienShalabi <hsonshb@gmail.com>
2023-07-08 22:47:51 +0300

    message

--- index.js
+++ index.js
@@ -45,10 +45,10 @@ function renderGame() {
     if (sum <= 20) {
         message = "Do you want to draw a new card? "
     } else if (sum === 21) {
-        message = "You've got Blackjack! "
+        message = "You've got Blackjack!"
         hasBlackJack = true
     } else {
-        message = "You're out of the game!ff "
+        message = "You're out of the game!"
         isAlive = false
     }
     messageEl.textContent = message
